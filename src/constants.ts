978ad17db3ddcbad1ff91feb09f90bd9157e7c71
admin <admin@example.com>
2021-07-04 13:40:58 +0200

    init

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,8 @@ export enum ChainId {
   ROPSTEN = 3,
   RINKEBY = 4,
   GÖRLI = 5,
-  KOVAN = 42
+  KOVAN = 42,
+  ETHERLITE = 111,
 }
 
 export enum TradeType {
@@ -22,7 +23,7 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0xac304Ca832eDf6036e07850f2b2B2707757827Fa'
 
 export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
 
--- src/entities/token.ts
+++ src/entities/token.ts
@@ -79,5 +79,6 @@ export const WETH = {
     'Wrapped Ether'
   ),
   [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
-  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether')
+  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.ETHERLITE]: new Token(ChainId.ETHERLITE, '0x71aBbd6B628Da7874E9861B6A9E5D67B10Eec729', 18, 'WETL', 'Wrapped Etherlite'),
 }
